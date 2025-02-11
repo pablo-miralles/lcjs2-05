32da8d1b705e9457946c71e156d53897e2c4afb4
Pablo Miralles <pablo@mowomo.com>
2025-02-12 00:43:00 +0100

    added muestraPuntuacion() y dameCarta()

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+let puntuacionTotal = 0;
+const contenedorPuntuacion = document.querySelector(".puntuacion-total");
+const dameCartaBtn = document.querySelector(".dame-carta");
+
+function muestraPuntuacion() {
+	if (contenedorPuntuacion) {
+		contenedorPuntuacion.innerHTML =
+			"Puntos: " + puntuacionTotal.toString();
+	}
+}
+
+function dameCarta() {
+	let carta = Math.floor(Math.random() * 10 + 1);
+
+	if (carta > 7) {
+		carta = 0.5;
+	}
+}
+
+document.addEventListener("DOMContentLoaded", muestraPuntuacion);
+
+if (dameCartaBtn) {
+	dameCartaBtn.addEventListener("click", dameCarta);
+}
